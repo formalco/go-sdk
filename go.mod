f0847283bb15e960077a17032ad117745dd87426
Loris Wintjens <loris@joinformal.com>
2024-04-06 08:56:07 +0200

    v1

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
-module github.com/formalco/go-sdk/v2
+module github.com/formalco/go-sdk
 
 go 1.22.0
 
 require github.com/dave/jennifer v1.7.0
+
+require buf.build/gen/go/formal/admin/connectrpc/go v1.15.0-20240208053541-2fc559d58b20.1 // indirect
--- sdk/sdk.go
+++ sdk/sdk.go
@@ -3,29 +3,44 @@ package sdk
 import (
 	"net/http"
 
-	corev1connect "buf.build/gen/go/formal/core/connectrpc/go/core/v1/corev1connect"
+	adminv1connect "buf.build/gen/go/formal/admin/connectrpc/go/admin/v1/adminv1connect"
 )
 
 const (
-	FORMAL_HOST_URL string = "https://v2api.formalcloud.net"
+	FORMAL_HOST_URL string = "https://adminv2.api.formalcloud.net"
 )
 
 type FormalSDK struct {
-	AuditServiceClient                  corev1connect.AuditServiceClient
-	GroupServiceClient                  corev1connect.GroupServiceClient
-	IntegrationBIServiceClient          corev1connect.IntegrationBIServiceClient
-	IntegrationCloudServiceClient       corev1connect.IntegrationCloudServiceClient
-	IntegrationDataCatalogServiceClient corev1connect.IntegrationDataCatalogServiceClient
-	IntegrationsLogServiceClient        corev1connect.IntegrationsLogServiceClient
-	IntegrationMfaServiceClient         corev1connect.IntegrationMfaServiceClient
-	InventoryServiceClient              corev1connect.InventoryServiceClient
-	PolicyServiceClient                 corev1connect.PolicyServiceClient
-	ResourceServiceClient               corev1connect.ResourceServiceClient
-	SatelliteServiceClient              corev1connect.SatelliteServiceClient
-	SessionServiceClient                corev1connect.SessionServiceClient
-	SidecarServiceClient                corev1connect.SidecarServiceClient
-	RowLevelTrackerServiceClient        corev1connect.RowLevelTrackerServiceClient
-	UserServiceClient                   corev1connect.UserServiceClient
+	DevServiceClient                   adminv1connect.DevServiceClient
+	AuditLogsServiceClient             adminv1connect.AuditLogsServiceClient
+	CordServiceClient                  adminv1connect.CordServiceClient
+	DataStoreServiceClient             adminv1connect.DataStoreServiceClient
+	FieldEncryptionPolicyServiceClient adminv1connect.FieldEncryptionPolicyServiceClient
+	FieldEncryptionServiceClient       adminv1connect.FieldEncryptionServiceClient
+	ETLServiceClient                   adminv1connect.ETLServiceClient
+	UserServiceClient                  adminv1connect.UserServiceClient
+	GroupServiceClient                 adminv1connect.GroupServiceClient
+	AppServiceClient                   adminv1connect.AppServiceClient
+	CloudServiceClient                 adminv1connect.CloudServiceClient
+	CodeRepositoryServiceClient        adminv1connect.CodeRepositoryServiceClient
+	DatahubServiceClient               adminv1connect.DatahubServiceClient
+	ExternalApiServiceClient           adminv1connect.ExternalApiServiceClient
+	GithubServiceClient                adminv1connect.GithubServiceClient
+	KmsServiceClient                   adminv1connect.KmsServiceClient
+	LogsServiceClient                  adminv1connect.LogsServiceClient
+	IntegrationMfaServiceClient        adminv1connect.IntegrationMfaServiceClient
+	SlackServiceClient                 adminv1connect.SlackServiceClient
+	SsoServiceClient                   adminv1connect.SsoServiceClient
+	InventoryServiceClient             adminv1connect.InventoryServiceClient
+	MetricsServiceClient               adminv1connect.MetricsServiceClient
+	NativeUserServiceClient            adminv1connect.NativeUserServiceClient
+	PermissionServiceClient            adminv1connect.PermissionServiceClient
+	PolicyServiceClient                adminv1connect.PolicyServiceClient
+	RegistryServiceClient              adminv1connect.RegistryServiceClient
+	SatelliteServiceClient             adminv1connect.SatelliteServiceClient
+	SearchServiceClient                adminv1connect.SearchServiceClient
+	SidecarServiceClient               adminv1connect.SidecarServiceClient
+	DSyncServiceClient                 adminv1connect.DSyncServiceClient
 }
 
 func New(apiKey string) *FormalSDK {
@@ -34,21 +49,36 @@ func New(apiKey string) *FormalSDK {
 		underlyingTransport: http.DefaultTransport,
 	}}
 	return &FormalSDK{
-		AuditServiceClient:                  corev1connect.NewAuditServiceClient(httpClient, FORMAL_HOST_URL),
-		GroupServiceClient:                  corev1connect.NewGroupServiceClient(httpClient, FORMAL_HOST_URL),
-		IntegrationBIServiceClient:          corev1connect.NewIntegrationBIServiceClient(httpClient, FORMAL_HOST_URL),
-		IntegrationCloudServiceClient:       corev1connect.NewIntegrationCloudServiceClient(httpClient, FORMAL_HOST_URL),
-		IntegrationDataCatalogServiceClient: corev1connect.NewIntegrationDataCatalogServiceClient(httpClient, FORMAL_HOST_URL),
-		IntegrationMfaServiceClient:         corev1connect.NewIntegrationMfaServiceClient(httpClient, FORMAL_HOST_URL),
-		IntegrationsLogServiceClient:        corev1connect.NewIntegrationsLogServiceClient(httpClient, FORMAL_HOST_URL),
-		InventoryServiceClient:              corev1connect.NewInventoryServiceClient(httpClient, FORMAL_HOST_URL),
-		PolicyServiceClient:                 corev1connect.NewPolicyServiceClient(httpClient, FORMAL_HOST_URL),
-		ResourceServiceClient:               corev1connect.NewResourceServiceClient(httpClient, FORMAL_HOST_URL),
-		RowLevelTrackerServiceClient:        corev1connect.NewRowLevelTrackerServiceClient(httpClient, FORMAL_HOST_URL),
-		SatelliteServiceClient:              corev1connect.NewSatelliteServiceClient(httpClient, FORMAL_HOST_URL),
-		SessionServiceClient:                corev1connect.NewSessionServiceClient(httpClient, FORMAL_HOST_URL),
-		SidecarServiceClient:                corev1connect.NewSidecarServiceClient(httpClient, FORMAL_HOST_URL),
-		UserServiceClient:                   corev1connect.NewUserServiceClient(httpClient, FORMAL_HOST_URL),
+		AppServiceClient:                   adminv1connect.NewAppServiceClient(httpClient, FORMAL_HOST_URL),
+		AuditLogsServiceClient:             adminv1connect.NewAuditLogsServiceClient(httpClient, FORMAL_HOST_URL),
+		CloudServiceClient:                 adminv1connect.NewCloudServiceClient(httpClient, FORMAL_HOST_URL),
+		CodeRepositoryServiceClient:        adminv1connect.NewCodeRepositoryServiceClient(httpClient, FORMAL_HOST_URL),
+		CordServiceClient:                  adminv1connect.NewCordServiceClient(httpClient, FORMAL_HOST_URL),
+		DSyncServiceClient:                 adminv1connect.NewDSyncServiceClient(httpClient, FORMAL_HOST_URL),
+		DataStoreServiceClient:             adminv1connect.NewDataStoreServiceClient(httpClient, FORMAL_HOST_URL),
+		DatahubServiceClient:               adminv1connect.NewDatahubServiceClient(httpClient, FORMAL_HOST_URL),
+		DevServiceClient:                   adminv1connect.NewDevServiceClient(httpClient, FORMAL_HOST_URL),
+		ETLServiceClient:                   adminv1connect.NewETLServiceClient(httpClient, FORMAL_HOST_URL),
+		ExternalApiServiceClient:           adminv1connect.NewExternalApiServiceClient(httpClient, FORMAL_HOST_URL),
+		FieldEncryptionPolicyServiceClient: adminv1connect.NewFieldEncryptionPolicyServiceClient(httpClient, FORMAL_HOST_URL),
+		FieldEncryptionServiceClient:       adminv1connect.NewFieldEncryptionServiceClient(httpClient, FORMAL_HOST_URL),
+		GithubServiceClient:                adminv1connect.NewGithubServiceClient(httpClient, FORMAL_HOST_URL),
+		GroupServiceClient:                 adminv1connect.NewGroupServiceClient(httpClient, FORMAL_HOST_URL),
+		IntegrationMfaServiceClient:        adminv1connect.NewIntegrationMfaServiceClient(httpClient, FORMAL_HOST_URL),
+		InventoryServiceClient:             adminv1connect.NewInventoryServiceClient(httpClient, FORMAL_HOST_URL),
+		KmsServiceClient:                   adminv1connect.NewKmsServiceClient(httpClient, FORMAL_HOST_URL),
+		LogsServiceClient:                  adminv1connect.NewLogsServiceClient(httpClient, FORMAL_HOST_URL),
+		MetricsServiceClient:               adminv1connect.NewMetricsServiceClient(httpClient, FORMAL_HOST_URL),
+		NativeUserServiceClient:            adminv1connect.NewNativeUserServiceClient(httpClient, FORMAL_HOST_URL),
+		PermissionServiceClient:            adminv1connect.NewPermissionServiceClient(httpClient, FORMAL_HOST_URL),
+		PolicyServiceClient:                adminv1connect.NewPolicyServiceClient(httpClient, FORMAL_HOST_URL),
+		RegistryServiceClient:              adminv1connect.NewRegistryServiceClient(httpClient, FORMAL_HOST_URL),
+		SatelliteServiceClient:             adminv1connect.NewSatelliteServiceClient(httpClient, FORMAL_HOST_URL),
+		SearchServiceClient:                adminv1connect.NewSearchServiceClient(httpClient, FORMAL_HOST_URL),
+		SidecarServiceClient:               adminv1connect.NewSidecarServiceClient(httpClient, FORMAL_HOST_URL),
+		SlackServiceClient:                 adminv1connect.NewSlackServiceClient(httpClient, FORMAL_HOST_URL),
+		SsoServiceClient:                   adminv1connect.NewSsoServiceClient(httpClient, FORMAL_HOST_URL),
+		UserServiceClient:                  adminv1connect.NewUserServiceClient(httpClient, FORMAL_HOST_URL),
 	}
 }
 
@@ -58,21 +88,36 @@ func NewWithUrl(apiKey string, url string) *FormalSDK {
 		underlyingTransport: http.DefaultTransport,
 	}}
 	return &FormalSDK{
-		AuditServiceClient:                  corev1connect.NewAuditServiceClient(httpClient, url),
-		GroupServiceClient:                  corev1connect.NewGroupServiceClient(httpClient, url),
-		IntegrationBIServiceClient:          corev1connect.NewIntegrationBIServiceClient(httpClient, url),
-		IntegrationCloudServiceClient:       corev1connect.NewIntegrationCloudServiceClient(httpClient, url),
-		IntegrationDataCatalogServiceClient: corev1connect.NewIntegrationDataCatalogServiceClient(httpClient, url),
-		IntegrationMfaServiceClient:         corev1connect.NewIntegrationMfaServiceClient(httpClient, url),
-		IntegrationsLogServiceClient:        corev1connect.NewIntegrationsLogServiceClient(httpClient, url),
-		InventoryServiceClient:              corev1connect.NewInventoryServiceClient(httpClient, url),
-		PolicyServiceClient:                 corev1connect.NewPolicyServiceClient(httpClient, url),
-		ResourceServiceClient:               corev1connect.NewResourceServiceClient(httpClient, url),
-		RowLevelTrackerServiceClient:        corev1connect.NewRowLevelTrackerServiceClient(httpClient, url),
-		SatelliteServiceClient:              corev1connect.NewSatelliteServiceClient(httpClient, url),
-		SessionServiceClient:                corev1connect.NewSessionServiceClient(httpClient, url),
-		SidecarServiceClient:                corev1connect.NewSidecarServiceClient(httpClient, url),
-		UserServiceClient:                   corev1connect.NewUserServiceClient(httpClient, url),
+		AppServiceClient:                   adminv1connect.NewAppServiceClient(httpClient, url),
+		AuditLogsServiceClient:             adminv1connect.NewAuditLogsServiceClient(httpClient, url),
+		CloudServiceClient:                 adminv1connect.NewCloudServiceClient(httpClient, url),
+		CodeRepositoryServiceClient:        adminv1connect.NewCodeRepositoryServiceClient(httpClient, url),
+		CordServiceClient:                  adminv1connect.NewCordServiceClient(httpClient, url),
+		DSyncServiceClient:                 adminv1connect.NewDSyncServiceClient(httpClient, url),
+		DataStoreServiceClient:             adminv1connect.NewDataStoreServiceClient(httpClient, url),
+		DatahubServiceClient:               adminv1connect.NewDatahubServiceClient(httpClient, url),
+		DevServiceClient:                   adminv1connect.NewDevServiceClient(httpClient, url),
+		ETLServiceClient:                   adminv1connect.NewETLServiceClient(httpClient, url),
+		ExternalApiServiceClient:           adminv1connect.NewExternalApiServiceClient(httpClient, url),
+		FieldEncryptionPolicyServiceClient: adminv1connect.NewFieldEncryptionPolicyServiceClient(httpClient, url),
+		FieldEncryptionServiceClient:       adminv1connect.NewFieldEncryptionServiceClient(httpClient, url),
+		GithubServiceClient:                adminv1connect.NewGithubServiceClient(httpClient, url),
+		GroupServiceClient:                 adminv1connect.NewGroupServiceClient(httpClient, url),
+		IntegrationMfaServiceClient:        adminv1connect.NewIntegrationMfaServiceClient(httpClient, url),
+		InventoryServiceClient:             adminv1connect.NewInventoryServiceClient(httpClient, url),
+		KmsServiceClient:                   adminv1connect.NewKmsServiceClient(httpClient, url),
+		LogsServiceClient:                  adminv1connect.NewLogsServiceClient(httpClient, url),
+		MetricsServiceClient:               adminv1connect.NewMetricsServiceClient(httpClient, url),
+		NativeUserServiceClient:            adminv1connect.NewNativeUserServiceClient(httpClient, url),
+		PermissionServiceClient:            adminv1connect.NewPermissionServiceClient(httpClient, url),
+		PolicyServiceClient:                adminv1connect.NewPolicyServiceClient(httpClient, url),
+		RegistryServiceClient:              adminv1connect.NewRegistryServiceClient(httpClient, url),
+		SatelliteServiceClient:             adminv1connect.NewSatelliteServiceClient(httpClient, url),
+		SearchServiceClient:                adminv1connect.NewSearchServiceClient(httpClient, url),
+		SidecarServiceClient:               adminv1connect.NewSidecarServiceClient(httpClient, url),
+		SlackServiceClient:                 adminv1connect.NewSlackServiceClient(httpClient, url),
+		SsoServiceClient:                   adminv1connect.NewSsoServiceClient(httpClient, url),
+		UserServiceClient:                  adminv1connect.NewUserServiceClient(httpClient, url),
 	}
 }
 
