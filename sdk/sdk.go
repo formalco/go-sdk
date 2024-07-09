b557a3e203200cfb5114d3a4281b906945de5924
Christopher Moreau <christopher@joinformal.com>
2024-07-09 10:14:46 +0200

    feat(sidecar-listeners): add new listener service in formal client

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	FORMAL_HOST_URL string = "https://v2api.formalcloud.net"
+	FormalHostUrl string = "https://v2api.formalcloud.net"
 )
 
 type FormalSDK struct {
@@ -20,6 +20,7 @@ type FormalSDK struct {
 	IntegrationMfaServiceClient         corev1connect.IntegrationMfaServiceClient
 	IntegrationMDMServiceClient         corev1connect.IntegrationMDMServiceClient
 	InventoryServiceClient              corev1connect.InventoryServiceClient
+	ListenersServiceClient              corev1connect.ListenerServiceClient
 	PoliciesServiceClient               corev1connect.PoliciesServiceClient
 	ResourceServiceClient               corev1connect.ResourceServiceClient
 	SatelliteServiceClient              corev1connect.SatelliteServiceClient
@@ -29,6 +30,7 @@ type FormalSDK struct {
 	UserServiceClient                   corev1connect.UserServiceClient
 }
 
+// New creates a new FormalSDK instance
 func New(apiKey string) *FormalSDK {
 	httpClient := &http.Client{
 		Transport: &transport{
@@ -38,25 +40,27 @@ func New(apiKey string) *FormalSDK {
 	}
 
 	return &FormalSDK{
-		LogsServiceClient:                   corev1connect.NewLogsServiceClient(httpClient, FORMAL_HOST_URL),
-		GroupServiceClient:                  corev1connect.NewGroupServiceClient(httpClient, FORMAL_HOST_URL),
-		IntegrationBIServiceClient:          corev1connect.NewIntegrationBIServiceClient(httpClient, FORMAL_HOST_URL),
-		IntegrationCloudServiceClient:       corev1connect.NewIntegrationCloudServiceClient(httpClient, FORMAL_HOST_URL),
-		IntegrationDataCatalogServiceClient: corev1connect.NewIntegrationDataCatalogServiceClient(httpClient, FORMAL_HOST_URL),
-		IntegrationMfaServiceClient:         corev1connect.NewIntegrationMfaServiceClient(httpClient, FORMAL_HOST_URL),
-		IntegrationsLogServiceClient:        corev1connect.NewIntegrationsLogServiceClient(httpClient, FORMAL_HOST_URL),
-		IntegrationMDMServiceClient:         corev1connect.NewIntegrationMDMServiceClient(httpClient, FORMAL_HOST_URL),
-		InventoryServiceClient:              corev1connect.NewInventoryServiceClient(httpClient, FORMAL_HOST_URL),
-		PoliciesServiceClient:               corev1connect.NewPoliciesServiceClient(httpClient, FORMAL_HOST_URL),
-		ResourceServiceClient:               corev1connect.NewResourceServiceClient(httpClient, FORMAL_HOST_URL),
-		TrackersServiceClient:               corev1connect.NewTrackersServiceClient(httpClient, FORMAL_HOST_URL),
-		SatelliteServiceClient:              corev1connect.NewSatelliteServiceClient(httpClient, FORMAL_HOST_URL),
-		SessionServiceClient:                corev1connect.NewSessionsServiceClient(httpClient, FORMAL_HOST_URL),
-		SidecarServiceClient:                corev1connect.NewSidecarServiceClient(httpClient, FORMAL_HOST_URL),
-		UserServiceClient:                   corev1connect.NewUserServiceClient(httpClient, FORMAL_HOST_URL),
+		LogsServiceClient:                   corev1connect.NewLogsServiceClient(httpClient, FormalHostUrl),
+		GroupServiceClient:                  corev1connect.NewGroupServiceClient(httpClient, FormalHostUrl),
+		IntegrationBIServiceClient:          corev1connect.NewIntegrationBIServiceClient(httpClient, FormalHostUrl),
+		IntegrationCloudServiceClient:       corev1connect.NewIntegrationCloudServiceClient(httpClient, FormalHostUrl),
+		IntegrationDataCatalogServiceClient: corev1connect.NewIntegrationDataCatalogServiceClient(httpClient, FormalHostUrl),
+		IntegrationMfaServiceClient:         corev1connect.NewIntegrationMfaServiceClient(httpClient, FormalHostUrl),
+		IntegrationsLogServiceClient:        corev1connect.NewIntegrationsLogServiceClient(httpClient, FormalHostUrl),
+		IntegrationMDMServiceClient:         corev1connect.NewIntegrationMDMServiceClient(httpClient, FormalHostUrl),
+		InventoryServiceClient:              corev1connect.NewInventoryServiceClient(httpClient, FormalHostUrl),
+		ListenersServiceClient:              corev1connect.NewListenerServiceClient(httpClient, FormalHostUrl),
+		PoliciesServiceClient:               corev1connect.NewPoliciesServiceClient(httpClient, FormalHostUrl),
+		ResourceServiceClient:               corev1connect.NewResourceServiceClient(httpClient, FormalHostUrl),
+		TrackersServiceClient:               corev1connect.NewTrackersServiceClient(httpClient, FormalHostUrl),
+		SatelliteServiceClient:              corev1connect.NewSatelliteServiceClient(httpClient, FormalHostUrl),
+		SessionServiceClient:                corev1connect.NewSessionsServiceClient(httpClient, FormalHostUrl),
+		SidecarServiceClient:                corev1connect.NewSidecarServiceClient(httpClient, FormalHostUrl),
+		UserServiceClient:                   corev1connect.NewUserServiceClient(httpClient, FormalHostUrl),
 	}
 }
 
+// NewWithUrl creates a new FormalSDK instance with a custom URL
 func NewWithUrl(apiKey string, url string) *FormalSDK {
 	httpClient := &http.Client{
 		Transport: &transport{
@@ -75,6 +79,7 @@ func NewWithUrl(apiKey string, url string) *FormalSDK {
 		IntegrationsLogServiceClient:        corev1connect.NewIntegrationsLogServiceClient(httpClient, url),
 		IntegrationMDMServiceClient:         corev1connect.NewIntegrationMDMServiceClient(httpClient, url),
 		InventoryServiceClient:              corev1connect.NewInventoryServiceClient(httpClient, url),
+		ListenersServiceClient:              corev1connect.NewListenerServiceClient(httpClient, url),
 		PoliciesServiceClient:               corev1connect.NewPoliciesServiceClient(httpClient, url),
 		ResourceServiceClient:               corev1connect.NewResourceServiceClient(httpClient, url),
 		TrackersServiceClient:               corev1connect.NewTrackersServiceClient(httpClient, url),
