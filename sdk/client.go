ca635548fddacb82917ffa7c282ff92a51f9fb83
Guillaume Gelin <contact@ramnes.eu>
2025-02-24 11:59:35 -0800

    Allow custom Formal API versions (#59)
    Use Formal API version 2025-02-24 by default

--- sdk/client.go
+++ sdk/client.go
@@ -5,17 +5,20 @@ import "net/http"
 type transport struct {
 	underlyingTransport http.RoundTripper
 	apiKey              string
+	apiVersion          string
 }
 
 func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Add("X-Api-Key", t.apiKey)
+	req.Header.Add("X-Formal-API-Version", "2025-02-24")
 	return t.underlyingTransport.RoundTrip(req)
 }
 
-func NewClient(apiKey string) *http.Client {
+func NewClient(apiKey, apiVersion string) *http.Client {
 	return &http.Client{
 		Transport: &transport{
 			apiKey:              apiKey,
+			apiVersion:          apiVersion,
 			underlyingTransport: http.DefaultTransport,
 		},
 	}
